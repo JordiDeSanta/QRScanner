fad3e09a99efc2ec3fffdf34eb03014558567caf
Jordilob <jordi.bravo.bad@gmail.com>
2021-03-23 18:12:27 -0300

    Importing google maps functionality

--- lib/src/pages/map_page.dart
+++ lib/src/pages/map_page.dart
@@ -1,16 +1,37 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:qrscanner/src/models/scan_model.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   const MapPage({Key key}) : super(key: key);
 
   @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  @override
   Widget build(BuildContext context) {
+    final CameraPosition _initialPoint = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962),
+      zoom: 14.4746,
+    );
     final ScanModel scan = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
       appBar: AppBar(
         title: Text(scan.value),
+      ),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _initialPoint,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
